8cb16c1188006e960477992d3a78de82f3d24335
Kai Wang <69681670+honeyland414@users.noreply.github.com>
2024-01-24 14:47:51 -0500

    add userservice (#1)
    * add pojo
    
    * springdatajpa
    
    * update gitignore

---
+++ UserService/src/main/java/com/example/userservice/config/SpringDataJPAConfig.java
@@ -0,0 +1,63 @@
+package com.example.userservice.config;
+
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.userservice.repository")
+@EnableTransactionManagement
+public class SpringDataJPAConfig {
+    @Value("jdbc:mysql://192.168.88.128:13306/cloud_demo")
+    private String url;
+
+    @Value("com.mysql.cj.jdbc.Driver")
+    private String driver;
+
+    @Value("root")
+    private String username;
+
+    @Value("111111")
+    private String password;
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.example.userservice.pojo");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
